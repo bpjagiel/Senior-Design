33c9e49773d0e2ca86ff3d9b8c68acb7bc436bb2
bpjagiel <bpjagiel@oakland.edu>
2014-09-22 23:47:58 -0400

    Update control system analysis and model

--- ControlSystemAnalysis/SystemModelSimulation.m
+++ ControlSystemAnalysis/SystemModelSimulation.m
@@ -1,13 +1,13 @@
 %%System Constants%%
-k = 100000;
-lambda = 100;
+k = 550;
+lambda = 1;
 I = 0.016363148;
 r = .050;
-m = .025;
+m = .01;
 
 %%State Space Model Matrices%%
-A = [0,1;-(k/((I/r^2)+m)) -(lambda/((I/r^2)+m))];
-B = [0;1/((I/r^2)+m)];
+A = [0,1;-(k/I) -(lambda/I)];
+B = [0;1/(I)];
 C = [0 1];
 D = [0:0];
 
@@ -25,3 +25,5 @@ u = [zeros(50,1)' ones(50,1)' zeros(50,1)' ones(50,1)' zeros(50,1)' 0];
 
 figure(3);
 lsim(ssmodel,u,t);
+
+clear all;
